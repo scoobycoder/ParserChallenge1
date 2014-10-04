5adb8250c9a7197737127a43a435e6aab9c09996
Tirthankar Bhattacharjee <feignfroggie@gmail.com>
2014-10-04 17:17:52 -0400

    add code with regex to parser

--- parser.rb
+++ parser.rb
@@ -1,3 +1,7 @@
 def word_in_string?(word, string)
   # implement with your code here
+  return :yes if /[^A-Za-z]#{word}[^A-Za-z]/.match("#{string}")
+  return :yes if /^#{word}[^A-Za-z]/.match("#{string}")
+  return :yes if /[^A-Za-z]#{word}$/.match("#{string}")
+  :no
 end
