5fd59ec2dc7e08f24fad14ea4313c87b7ca0965c
Benjamin Madueme <bmadueme@gmail.com>
2014-12-07 22:44:49 -0500

    Implemented the word_in_string? function in parser.rb

--- parser.rb
+++ parser.rb
@@ -1,3 +1,5 @@
+#Consulted http://www.regular-expressions.info/wordboundaries.html
 def word_in_string?(word, string)
-  # implement with your code here
+  #Need to replace underscores with spaces if we want them treated as non-word chars
+  string.gsub("_", " ").match(/\b#{word}\b/) ? :yes : :no
 end
