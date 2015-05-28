c75a16070a25d7d411b0abdbf68b83617485c2c3
LoRyder1 <lpnag@CMDVMOSXLT075.local>
2015-05-28 13:33:37 -0400

    finished parser

--- parser.rb
+++ parser.rb
@@ -1,3 +1,11 @@
 def word_in_string?(word, string)
-  # implement with your code here
+	string_parse = string.gsub(/_|-/,' ')
+	string_parse.split.each do |x|
+		return :yes if x == word
+	end
+	return :no
 end
+
+# p word_in_string?("grow", "growler")
+# p word_in_string?("grow", "miracle grow")
+p word_in_string?("book", "book_shelf")
