419555966659bccfe9148bec6a3dad4c825f67df
Bryon Wilkins <bwilkins@CMDVMOSXLT082.local>
2015-06-08 14:06:29 -0400

    Completed Challenge

--- parser.rb
+++ parser.rb
@@ -1,3 +1,10 @@
 def word_in_string?(word, string)
   # implement with your code here
+  words = string.scan /(^|[^a-zA-Z])#{Regexp.quote(word)}($|[^a-zA-Z])/
+  case words.count>0
+  when true
+    :yes
+  else
+    :no
+  end
 end
