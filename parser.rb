08f706be1d42f58eb5ac196baa61d0bea4c277ee
Priya <priya.ramesh6@gmail.com>
2016-01-14 10:19:22 -0500

    Completed

--- parser.rb
+++ parser.rb
@@ -1,3 +1,9 @@
 def word_in_string?(word, string)
-  # implement with your code here
+  
+  string_store = string.gsub(/[_-]/,' ').split(/\s/)
+  if string_store.include? word
+  	return :yes.to_sym
+  else
+  	return :no.to_sym
+  end
 end
